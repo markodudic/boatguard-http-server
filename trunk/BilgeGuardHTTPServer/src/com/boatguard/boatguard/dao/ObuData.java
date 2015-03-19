e9db83d6fa95d07455e6e5b69ab07239b1386397
marko <marko@e7fa515c-484e-0410-8bce-fe8274a4e620>
2015-03-19 21:28:52 +0000

    battery settings update

--- trunk/BilgeGuardHTTPServer/src/com/boatguard/boatguard/dao/ObuData.java
+++ trunk/BilgeGuardHTTPServer/src/com/boatguard/boatguard/dao/ObuData.java
@@ -351,8 +351,8 @@ public class ObuData {
 			    				stateValue = "0";
 			    			}
 			    			else {
-				    			HashMap<String, BatterySetting> batterySetting = Cache.batterySettings.get(obu.getId_battery_settings());
-				    			if (batterySetting.get(stateValue) == null) {
+			    				HashMap<String, BatterySetting> batterySetting = Cache.batterySettings.get(obu.getId_battery_settings());
+			    				if (batterySetting.get(stateValue) == null) {
 				    				//interpoliram vrednost hexa
 				    				Set<String> keys = batterySetting.keySet();
 				    				long min_diff = 99999;
@@ -370,13 +370,13 @@ public class ObuData {
 				    				}
 				    				stateValue = newValue;
 				    			}
-				    			String stateNapetost = stateValue;
-				    			stateValue = Util.hexaToDec(stateValue)/batterySetting.get(stateValue).getKoef() +"";
+			    				//String stateNapetost = stateValue;
+				    			//stateValue = Util.hexaToDec(stateValue)/batterySetting.get(stateValue).getKoef() +"";
 				    			
 				    			//procente izracunam iz napetosti in ne iz porabe
-				    			String procent = batterySetting.get(stateNapetost).getPercent() +"";
+				    			String procent = batterySetting.get(stateValue).getPercent() +"";
 				    			Map<Integer, String> obuSettings = obu.getSettings();
-				    			
+			    				
 				    			int empty = Integer.parseInt(obuSettings.get(Constant.OBU_SETTINGS_BATTERY_ALARM_LEVEL_VALUE));
 				    			sql = "insert into states_data (id_state, id_obu, value, date_state) " + 
 						    	   		"values ('" + Constant.STATE_ACCU_EMPTY_VALUE + "', " + obu.getUid() + ", '" + (Integer.parseInt(procent)<empty?Constant.BATTERY_EMPTY_VALUE:"0") + "', '" + dateState + "')";
