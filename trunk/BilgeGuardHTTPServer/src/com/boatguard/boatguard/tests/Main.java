c7b65898f31e61910e952645a9debea0f2cbc8b2
marko <marko@e7fa515c-484e-0410-8bce-fe8274a4e620>
2015-03-25 10:39:40 +0000

    Test

--- trunk/BilgeGuardHTTPServer/src/com/boatguard/boatguard/tests/Main.java
+++ trunk/BilgeGuardHTTPServer/src/com/boatguard/boatguard/tests/Main.java
@@ -12,6 +12,7 @@ import org.json.JSONObject;
 import org.json.JSONTokener;
 
 import com.boatguard.boatguard.objects.ObuAlarm;
+import com.boatguard.boatguard.objects.ObuSetting;
 import com.boatguard.boatguard.objects.ObuState;
 import com.boatguard.boatguard.util.Constant;
 import com.google.gson.Gson;
@@ -19,101 +20,132 @@ import com.google.gson.Gson;
 
 public class Main {
 
-	private final String USER_AGENT = "Mozilla/5.0";
-	private String server = "http://93.103.12.155:8080/boatguard/";
-	private String serialObu = "12345";
-	private String idObu = "1";
-	static Main main = new Main();
-	public static HashMap<Integer,ObuState> obuStates = new HashMap<Integer,ObuState>(){};
-	public static HashMap<Integer,ObuAlarm> obuAlarms = new HashMap<Integer,ObuAlarm>(){};
+	private final String server = "http://93.103.12.155:8080/boatguard/";
+	private final String serialObu = "12345";
+	private final String idObu = "1";
 	
-	private static int bilgePumpState = Constant.PUMP_PUMPING_VALUE;
-    
+	private final static int bilgePumpState = Constant.PUMP_CLODGED_VALUE; //0-ok;1-pumping;2-clodged;3-dry;4-servis
+	private final static int anchorEnabledState = Constant.ANCHOR_ENABLED_VALUE; //1-da;0-NE
+	private final static int anchorDriftingState = Constant.ANCHOR_ENABLED_VALUE; //1-da;0-NE
+	
+	private final static String geoFenceLat = "01402.5046";
+	private final static String geoFenceLon = "4626.0058";
+	private final static int geoFenceEnabled = Constant.GEO_FENCE_ENABLED_VALUE; //1-da;0-NE
+	private final static int geoFenceResult = Constant.GEO_FENCE_ALARM_VALUE; //0-disabled;1-enabled;2-alarm
+   
+	private static Main main = new Main();
+	private static HashMap<Integer,ObuState> obuStates = new HashMap<Integer,ObuState>(){};
+	private static HashMap<Integer,ObuAlarm> obuAlarms = new HashMap<Integer,ObuAlarm>(){};
+	private static HashMap<Integer,ObuSetting> obuSettings = new HashMap<Integer,ObuSetting>(){};
+		
+	
     public static void main(String[] args) {
         System.out.println("START!");
-        main.setBilgePump(bilgePumpState); //0-ok;1-pumping;2-clodged;3-dry;4-servis
         
+        main.setSettings(geoFenceEnabled, geoFenceLat, geoFenceLon);
+        main.setStates(bilgePumpState, anchorEnabledState==1?"D":"N", anchorDriftingState==1?"D":"N", geoFenceLat, geoFenceLon); 
         main.getObuData();
-        
+		System.out.println("*****TEST RESULTS*****");
         main.checkBilgePump();
+        main.checkAnchor();
+        main.checkGeoFence();
     }
-    
-	private String sendPost(String url) {
-    	try {
-		 
-			URL obj = new URL(url);
-			HttpURLConnection con = (HttpURLConnection) obj.openConnection();
-	 
-			//add reuqest header
-			con.setRequestMethod("POST");
-			//con.setRequestProperty("User-Agent", USER_AGENT);
-			//con.setRequestProperty("Accept-Language", "en-US,en;q=0.5");
-	 
-			// Send post request
-			con.setDoOutput(true);
-			DataOutputStream wr = new DataOutputStream(con.getOutputStream());
-			//wr.writeBytes(urlParameters);
-			wr.flush();
-			wr.close();
-	 
-			int responseCode = con.getResponseCode();
-			System.out.println("\nSending 'POST' request to URL : " + url);
-			System.out.println("Response Code : " + responseCode);
-	 
-			BufferedReader in = new BufferedReader(
-			        new InputStreamReader(con.getInputStream()));
-			String inputLine;
-			StringBuffer response = new StringBuffer();
-	 
-			while ((inputLine = in.readLine()) != null) {
-				response.append(inputLine);
-			}
-			in.close();
-	 
-			//print result
-			System.out.println(response.toString());
-			return response.toString();
-		} catch (Exception e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-			return null;
-		} 
-	} 
-	
-    private void setBilgePump(int state) {
-		System.out.println("*****BILGE PUMP TEST*****");
-    	String url = server + "setdata?serial="+serialObu+"&data=4519.2591,N,01333.1484,E,1,16,0,"+state+",N,D,N,D,0000,000000,03FF,00,00,";
+
+    private void setStates(int pumpState, String anchorState, String anchorDrifting, String lat, String lon) {
+		System.out.println("*****TEST START*****");
+    	String url = server + "setdata?serial="+serialObu+"&data="+lon+",N,"+lat+",E,1,16,0,"+pumpState+",N,"+anchorState+","+anchorDrifting+",D,0000,000000,03FF,00,00,";
 		main.sendPost(url);
     }
-    
+
+    private void setSettings(int geoFenceEnabled, String geoFenceLat, String geoFenceLon) {
+		System.out.println("*****SETTINGS START*****");
+    	String url = server + "getobusettings?obuid="+idObu;
+    	getSettings(main.sendPost(url));
+    }
+
     private void checkBilgePump() {
     	ObuState obuState = obuStates.get(Constant.STATE_PUMP_STATE_VALUE);
     	if (Integer.parseInt(obuState.getValue()) == Constant.PUMP_OK_VALUE) {
-    		System.out.println(Constant.PUMP_OK);
+    		System.out.println(bilgePumpState+":"+Constant.PUMP_OK);
     	}
     	else if (Integer.parseInt(obuState.getValue()) == Constant.PUMP_PUMPING_VALUE) {
-    		System.out.println(Constant.PUMP_PUMPING);
+    		System.out.println(bilgePumpState+":"+Constant.PUMP_PUMPING);
     	}
     	else if (Integer.parseInt(obuState.getValue()) == Constant.PUMP_CLODGED_VALUE) {
-    		System.out.println(Constant.PUMP_CLODGED);
+    		System.out.println(bilgePumpState+":"+Constant.PUMP_CLODGED);
     	}
     	else if (Integer.parseInt(obuState.getValue()) == Constant.PUMP_DEMAGED_VALUE) {
-    		System.out.println(Constant.PUMP_DEMAGED);
+    		System.out.println(bilgePumpState+":"+Constant.PUMP_DEMAGED);
     	}
     	if (Integer.parseInt(obuState.getValue()) == bilgePumpState) {
     		System.out.println("*****BILGE PUMP TEST OK*****");
     	}
     	else {
-    		System.out.println("*****BILGEBILGE PUMP TEST NI OK*****");    		
+    		System.out.println("!!!!!!BILGEBILGE PUMP TEST NI OK!!!!!!");    		
     	}
     }
 
+    private void checkAnchor() {
+    	ObuState anchorState = obuStates.get(Constant.STATE_ANCHOR_VALUE);
+    	if (Integer.parseInt(anchorState.getValue()) == Constant.ANCHOR_ENABLED_VALUE) {
+    		System.out.println(anchorEnabledState+":"+Constant.ANCHOR_ENABLED);
+    	}
+    	else if (Integer.parseInt(anchorState.getValue()) != Constant.ANCHOR_ENABLED_VALUE) {
+    		System.out.println(anchorEnabledState+": NOT " + Constant.ANCHOR_ENABLED);
+    	}    	
+    	
+    	ObuState anchorDrifting = obuStates.get(Constant.STATE_ANCHOR_DRIFTING_VALUE);
+    	if (Integer.parseInt(anchorState.getValue()) == Constant.ANCHOR_ENABLED_VALUE) {
+    		System.out.println(anchorDriftingState+":"+Constant.STATE_ANCHOR_DRIFTING);
+    	}
+    	else if (Integer.parseInt(anchorState.getValue()) != Constant.ANCHOR_ENABLED_VALUE) {
+    		System.out.println(anchorDriftingState+": NOT " + Constant.STATE_ANCHOR_DRIFTING);
+    	}
+    	
+    	if ((Integer.parseInt(anchorState.getValue()) == anchorEnabledState) && 
+    		(Integer.parseInt(anchorDrifting.getValue()) == anchorDriftingState)) {
+    		System.out.println("*****ANCHOR TEST OK*****");
+    	}
+    	else {
+    		System.out.println("!!!!!!ANCHOR TEST NI OK!!!!!!");    		
+    	}
+    }
+
+    private void checkGeoFence() {
+    	if (Integer.parseInt(obuSettings.get(Constant.STATE_GEO_FENCE_VALUE).getValue()) == Constant.GEO_FENCE_ENABLED_VALUE) {
+    		System.out.println("GEO FENCE SETTING: "+Constant.GEO_FENCE_ENABLED);   		
+    	}
+    	else {
+    		System.out.println("GEO FENCE SETTING: "+Constant.GEO_FENCE_DISABLED);    		
+    	}
+    	
+    	ObuState geoFenceState = obuStates.get(Constant.STATE_GEO_FENCE_VALUE);
+    	if (Integer.parseInt(geoFenceState.getValue()) == Constant.GEO_FENCE_ENABLED_VALUE) {
+    		System.out.println(geoFenceEnabled+":"+Constant.GEO_FENCE_ENABLED);
+    	}
+    	else if (Integer.parseInt(geoFenceState.getValue()) == Constant.GEO_FENCE_DISABLED_VALUE) {
+    		System.out.println(geoFenceEnabled+": " + Constant.GEO_FENCE_DISABLED);
+    	}
+    	else if (Integer.parseInt(geoFenceState.getValue()) == Constant.GEO_FENCE_ALARM_VALUE) {
+    		System.out.println(geoFenceEnabled+": " + Constant.GEO_FENCE_ALARM);
+    	}
+    	
+    	
+    	
+    	if (Integer.parseInt(geoFenceState.getValue()) == geoFenceResult) {
+    		System.out.println("*****GEO FENCE TEST OK*****");
+    	}
+    	else {
+    		System.out.println("!!!!!!GEO FENCE  TEST NI OK!!!!!!");    		
+    	}
+    }
+    
     private void getObuData() {
     	String url = server + "getdata?obuid="+idObu;
     	getData(main.sendPost(url));
     }    
     
-    public void getData(String res) {
+    private void getData(String res) {
 		try {
 			Gson gson = new Gson();	
 			JSONObject jRes = (JSONObject)new JSONTokener(res).nextValue();
@@ -137,4 +169,60 @@ public class Main {
 	   	}
     }
 
+    private void getSettings(String res) {
+		try {
+			Gson gson = new Gson();	
+            JSONArray jsonObuSettings = (JSONArray)new JSONTokener(res).nextValue();
+	   		obuSettings.clear();
+	   		for (int i=0; i< jsonObuSettings.length(); i++) {
+	   			ObuSetting obuSetting = gson.fromJson(jsonObuSettings.get(i).toString(), ObuSetting.class);
+	   			//System.out.println(obuSetting.toString());
+	   			obuSettings.put(obuSetting.getId_setting(), obuSetting);
+	   		}  	        
+	   		
+        } catch (Exception e) {
+    	   	e.printStackTrace();
+        	e.getLocalizedMessage();
+	   	}
+    }
+   
+	private String sendPost(String url) {
+    	try {
+		 
+			URL obj = new URL(url);
+			HttpURLConnection con = (HttpURLConnection) obj.openConnection();
+	 
+			//add reuqest header
+			con.setRequestMethod("POST");
+			
+			// Send post request
+			con.setDoOutput(true);
+			DataOutputStream wr = new DataOutputStream(con.getOutputStream());
+			//wr.writeBytes(urlParameters);
+			wr.flush();
+			wr.close();
+	 
+			int responseCode = con.getResponseCode();
+			System.out.println("\nSending 'POST' request to URL : " + url);
+			System.out.println("Response Code : " + responseCode);
+	 
+			BufferedReader in = new BufferedReader(
+			        new InputStreamReader(con.getInputStream()));
+			String inputLine;
+			StringBuffer response = new StringBuffer();
+	 
+			while ((inputLine = in.readLine()) != null) {
+				response.append(inputLine);
+			}
+			in.close();
+	 
+			//print result
+			System.out.println("Response : " + response.toString());
+			return response.toString();
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			return null;
+		} 
+	} 
 }
